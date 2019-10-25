f849cb94aab8ca13a94c83f6b2f1c4d2e9540a57
kred420 <kred420@gmail.com>
2019-10-25 15:30:04 -0500

    Add files via upload
    This is a template

---
+++ CSCE1030Template.cpp
@@ -0,0 +1,19 @@
+/* Author:	Kevin Day (kevinday@my.unt.edu)
+   Date: 
+   Instructor: Pradhumna Shrestha
+   Description: 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    //Displaying header
+    cout << "+----------------------------------------------+\n";
+    cout << "|       Computer Science and Engineering       |\n";
+    cout << "|        CSCE 1030 - Computer Science I        |\n";
+    cout << "|    Kevin Day kmd0282 kevinday@my.unt.edu     |\n";
+    cout << "+----------------------------------------------+\n";
+    //
+}
